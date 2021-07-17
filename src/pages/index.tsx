174b8d994595deba76a412061fa3ae161e5ed090
jiroor <thx.1by1.or25@gmail.com>
2021-07-17 09:46:37 +0900

    feat(Home): add my data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,61 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          jiroor
-        </h1>
-      </main>
+        <section className={styles.section}>
+          <h1 className={styles.heading1}>
+            jiroor
+          </h1>
+        </section>
 
-      <Link href='/recognition'>
-        Recognition
-      </Link>
+        <section className={styles.section}>
+          <h2 className={styles.heading2}>
+            Profile
+          </h2>
+          <div>
+            安田善樹
+            やすだよしき
+            よしお・じろー
+          </div>
+        </section>
+
+        <section className={styles.section}>
+          <h2 className={styles.heading2}>
+            Career
+          </h2>
+          <div>
+            Webフロントエンジニア
+
+            2013年5月 天空のクリスタリア
+            2015年5月 スクールファンファーレ
+            2016年10月 BF
+            2018年10月 mogg
+            2019年10月 OPENREC.tv
+          </div>
+        </section>
+
+        <section className={styles.section}>
+          <h2 className={styles.heading2}>
+            Skill
+          </h2>
+          <div>
+            HTML, CSS, JavaScript
+            SCSS, PostCSS
+            TypeScript
+            React, Vue.js, AngularJS
+          </div>
+        </section>
+
+        <section className={styles.section}>
+          <h2 className={styles.heading2}>
+            Tools
+          </h2>
+          <div>
+            <Link href='/recognition'>
+              Recognition
+            </Link>
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
